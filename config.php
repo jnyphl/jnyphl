8fe5c9651dd2d0531bdc6c8efa604fbdb4567602
Jenny Porada <jennyporada@gmail.com>
2018-12-22 01:59:02 -0500

    Jigsaw static site

---
+++ config.php
@@ -0,0 +1,50 @@
+<?php
+
+return [
+    'baseUrl' => '',
+    'production' => false,
+    'siteName' => 'JNYPHL',
+    'siteDescription' => 'Jenny and Phil',
+    'siteAuthor' => 'Jenny and Phil',
+
+    // collections
+    'collections' => [
+        'posts' => [
+            'author' => 'Jenny', // Default author, if not provided in a post
+            'sort' => '-date',
+            'path' => 'blog/{filename}',
+        ],
+        'categories' => [
+            'path' => '/blog/categories/{filename}',
+            'posts' => function ($page, $allPosts) {
+                return $allPosts->filter(function ($post) use ($page) {
+                    return $post->categories ? in_array($page->getFilename(), $post->categories, true) : false;
+                });
+            },
+        ],
+    ],
+
+    // helpers
+    'getDate' => function ($page) {
+        return Datetime::createFromFormat('U', $page->date);
+    },
+    'excerpt' => function ($page, $length = 255) {
+        $cleaned = strip_tags(
+            preg_replace(['/<pre>[\w\W]*?<\/pre>/', '/<h\d>[\w\W]*?<\/h\d>/'], '', $page->getContent()),
+            '<code>'
+        );
+
+        $truncated = substr($cleaned, 0, $length);
+
+        if (substr_count($truncated, '<code>') > substr_count($truncated, '</code>')) {
+            $truncated .= '</code>';
+        }
+
+        return strlen($cleaned) > $length
+            ? preg_replace('/\s+?(\S+)?$/', '', $truncated) . '...'
+            : $cleaned;
+    },
+    'isActive' => function ($page, $path) {
+        return ends_with(trimPath($page->getPath()), trimPath($path));
+    },
+];
---
+++ source/_posts/jenny.blade.php
@@ -0,0 +1,16 @@
+---
+extends: _layouts.post
+section: content
+title: Jenny's post
+date: 2018-12-29
+description: This is your first blog post.
+cover_image: /assets/img/post-cover-image-2.png
+---
+
+@section ('content')
+
+    <h2>This is a post without markdown</h2>
+
+    <p>la da da dee dee</p>
+
+@endsection
