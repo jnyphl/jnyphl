6fe6dd28464e2f6ae33dd89886b8eecd1a84b9fb
Jenny Porada <jennyporada@gmail.com>
2018-12-23 12:36:47 -0500

    Farmily post update

--- source/_posts/porada-farmily.blade.php
+++ source/_posts/porada-farmily.blade.php
@@ -4,7 +4,7 @@ section: content
 title: 'Porada Farmily: We bought a house!'
 date: 2018-01-29
 description: This is your first blog post.
-cover_image: /assets/img/we-bought-a-house/house-front-snow.jpg
+cover_image: /assets/images/2018-01-29-we-bought-a-house/house-front-snow.jpg
 featured: true
 categories: [featured]
 ---
@@ -13,8 +13,37 @@ categories: [featured]
 
     <h2>We got a house</h2>
 
-    <p>la da da dee dee</p>
+    <div class="flex -mx-4">
+        <div class=" mx-4">
+            <img src="/assets/images/2018-01-29-we-bought-a-house/keys.jpg">
+            <div>Keys!</div>
+        </div>
+        <div class="mx-4">
+            <img src="/assets/images/2018-01-29-we-bought-a-house/chicken-coop.jpg">
+            <div>Gwen left an old photo of the chicken cook on a shelf in the kitchen</div>
+        </div>
+    </div>
 
-    <img src="/assets/img/we-bought-a-house/house-front-lising.jpg">
+    <h2>Meet our neigh-bors</h2>
+    <div class="flex -mx-4">
+        <div class="mx-4">
+            <img src="/assets/images/2018-01-29-we-bought-a-house/neighbor-horses.jpg">
+        </div>
+        <div class="mx-4">
+            <img src="/assets/images/2018-01-29-we-bought-a-house/neighbor-horses-2.jpg">
+        </div>
+    </div>
+
+    <h2>Photots from the lising</h2>
+    <div class="flex -mx-4">
+        <div class="mx-4">
+            <img src="/assets/images/2018-01-29-we-bought-a-house/house-front-lising.jpg">
+            <div>Front of the house</div>
+        </div>
+        <div class="mx-4">
+            <img src="/assets/images/2018-01-29-we-bought-a-house/house-back.jpg">
+            <div>Back of the house</div>
+        </div>
+    </div>
 
 @endsection
