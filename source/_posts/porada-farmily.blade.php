568aa2579d1741d3541003d47b1b988d75904b88
Jenny Porada <jennyporada@gmail.com>
2018-12-23 11:21:47 -0500

    Post update

--- source/_posts/porada-farmily.blade.php
+++ source/_posts/porada-farmily.blade.php
@@ -1,10 +1,10 @@
 ---
 extends: _layouts.post
 section: content
-title: Porada Farmily
+title: 'Porada Farmily: We bought a house!'
 date: 2018-01-29
 description: This is your first blog post.
-cover_image: /assets/img/post-cover-image-2.png
+cover_image: /assets/img/we-bought-a-house/house-front-snow.jpg
 featured: true
 categories: [featured]
 ---
@@ -14,5 +14,7 @@ categories: [featured]
     <h2>We got a house</h2>
 
     <p>la da da dee dee</p>
+
+    <img src="/assets/img/we-bought-a-house/house-front-lising.jpg">
 
 @endsection
