1a020ba0c70f571443f5df2823a28ed732e16722
Jenny Porada <jennyporada@gmail.com>
2018-12-23 11:29:14 -0500

    New post

---
+++ source/_posts/holiday-cards.blade.php
@@ -0,0 +1,16 @@
+---
+extends: _layouts.post
+section: content
+title: Holiday cards 2018
+date: 2018-12-01
+description: This is your first blog post.
+cover_image: /assets/img/post-cover-image-2.png
+featured: true
+categories: [featured]
+---
+
+@section ('content')
+
+    <p>la da da dee dee</p>
+
+@endsection
