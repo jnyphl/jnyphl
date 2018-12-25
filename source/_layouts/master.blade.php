bf5e42d5e8497b83971d90997be8f2087a0b71eb
Jenny Porada <jennyporada@gmail.com>
2018-12-25 09:58:12 -0500

    SVG logo

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -32,9 +32,8 @@
                 <div class="flex items-center w-full">
                     <div class="flex items-center px-4">
                         <a href="/" title="{{ $page->siteName }} home" class="inline-flex items-center">
-                            <h1 class="border-2 border-forest-dark hover:border-forest-light p-2 my-0 text-sm font-mono text-forest-dark hover:text-forest-light tracking-wide leading-none">
-                                <span class="block">JNY</span>
-                                <span class="block">PHL</span>
+                            <h1 class="w-16">
+                                <svg class="text-forest fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 300 300"><path d="M140.2 211.8h21.2v31.5H177v-78.6h-15.6v31.5h-21.2v-31.5h-15.6v78.6h15.6zM72 211.8h36.6v-47.2H56.5v78.7H72v-31.5zm21-31.6v16H72v-16h21zM210.2 164.7h-15.5v78.6h48.8v-15.5h-33.3z"/><g><path d="M140.2 89.9l21.2 45.4H177V56.7h-15.6v45.4l-21.2-45.4h-15.6v78.6h15.6zM56.5 135.3h52.1V56.7H93v63.1H72V97.2H56.5v25zM204.5 135.3h15.6l8.9-29.9 14.5-48.7H228l-8.4 27.9L207 56.7h-15.6l22.1 48.7z"/></g><path d="M0 0v300h300V0H0zm284.4 285.6H15.6V14.4h268.9v271.2z"/></svg>
                             </h1>
                         </a>
                     </div>
