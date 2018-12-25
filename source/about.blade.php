ce77d628dae4e5af6f9e34f4bfba14ddd0ed4743
Jenny Porada <jennyporada@gmail.com>
2018-12-25 09:58:27 -0500

    Updating about page

--- source/about.blade.php
+++ source/about.blade.php
@@ -12,9 +12,7 @@
 
     <img src="/assets/images/about/us.jpg" alt="About image" class="flex rounded-full h-64 w-64 bg-contain mx-auto md:float-right my-6 md:ml-10">
 
-    <p class="mb-6">This is where you can give a little more information about yourself or site. If you'd like to change the structure of this page, you can find the file at <code>source/about.blade.php</code></p>
+    <p><a href="https://philporada.com">https://philporada.com</a></p>
+    <p><a href="https://jennyingles.com">https://jennyingles.com</a></p>
 
-    <p class="mb-6">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum officia dolorem accusantium veniam quae, possimus, temporibus explicabo magni voluptas. fugit natus deserunt atque veniam possimus earum harum itaque est!</p>
-
-    <p class="mb-6">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum officia dolorem accusantium veniam quae, possimus, temporibus explicabo magni voluptas. fugit natus deserunt atque veniam!</p>
 @endsection
