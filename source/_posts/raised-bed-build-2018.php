5e30b58e169e5c05bd9bc914cf881209b21ba9ae
Phil Porada <philporada@gmail.com>
2018-12-30 17:49:11 -0500

    Started the raised bed build post

---
+++ source/_posts/raised-bed-build-2018.php
@@ -0,0 +1,23 @@
+---
+extends: _layouts.post
+section: content
+title: Getting Started
+date: 2018-12-30
+description: Getting started with the Jigsaw blog starter template
+cover_image: /assets/images/raised-bed-build-2018/10-mulch-begins.png
+featured: false
+author: Phil
+categories: [garden,building,outside]
+---
+@section ('content')
+    <div class="flex flex-wrap -mx-4 mt-6">
+        <div class="px-4 mb-8 w-1/2">
+            <img src="/assets/images/raised-bed-build-2018/01-built-some-boxes.png">
+            <p class="my-2">Thing 1</p>
+        </div>
+        <div class="px-4 mb-8 w-1/2">
+            <img src="/assets/images/raised-bed-build-2018/02-rototilling.png">
+            <p class="my-2">Thing 2</p>
+        </div>
+    </div>
+@endsection
