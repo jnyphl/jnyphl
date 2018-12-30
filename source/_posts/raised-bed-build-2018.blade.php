57685561727ab29d417a0eb2933a226408791de9
Phil Porada <philporada@gmail.com>
2018-12-30 18:02:16 -0500

    Updated raised bed blog post

--- source/_posts/raised-bed-build-2018.blade.php
+++ source/_posts/raised-bed-build-2018.blade.php
@@ -1,10 +1,10 @@
 ---
 extends: _layouts.post
 section: content
-title: Getting Started
+title: Raised Garden Bed Build 2018
 date: 2018-12-30
-description: Getting started with the Jigsaw blog starter template
-cover_image: /assets/images/raised-bed-build-2018/10-mulch-begins.png
+description: We built raised garden beds in a plot bigger than our old house!
+cover_image: /assets/images/raised-bed-build-2018/10-mulch-begins.jpg
 featured: false
 author: Phil
 categories: [garden,building,outside]
@@ -12,11 +12,11 @@ categories: [garden,building,outside]
 @section ('content')
     <div class="flex flex-wrap -mx-4 mt-6">
         <div class="px-4 mb-8 w-1/2">
-            <img src="/assets/images/raised-bed-build-2018/01-built-some-boxes.png">
+            <img src="/assets/images/raised-bed-build-2018/01-built-some-boxes.jpg">
             <p class="my-2">We built some boxes!</p>
         </div>
         <div class="px-4 mb-8 w-1/2">
-            <img src="/assets/images/raised-bed-build-2018/02-rototilling.png">
+            <img src="/assets/images/raised-bed-build-2018/02-rototilling.jpg">
             <p class="my-2">We began roto-tilling a plot larger than our previous house.</p>
         </div>
     </div>
