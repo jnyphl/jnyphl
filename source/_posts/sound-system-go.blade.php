c0e00abd2c38d4c280ddb1fd617309452f0bb47b
Phil Porada <philporada@gmail.com>
2019-02-01 22:26:11 -0500

    Updated the sound system post

--- source/_posts/sound-system-go.blade.php
+++ source/_posts/sound-system-go.blade.php
@@ -7,12 +7,29 @@ description:  Sound system gonna bring me back up. Yeah, one thing that I can de
 cover_image: /assets/images/2018-09-01-sound-system/sound-system.jpg
 featured: false
 author: Phil
-override_excerpt: If you want to get the feeling and you want to get it right, Then the music's gotta be loud, For when the music hits I feel no pain at all.
+override_excerpt: If you want to get the feeling and you want to get it right, then the music's gotta be loud, For when the music hits I feel no pain at all.
 categories: [house,barn,music,raspberry pi]
 ---
 @section ('content')
-
     <h2>Record player, speakers, and amp</h2>
+    <p>
+    We purchased this setup as a Christmas present to our newly married selves back in 2016.
+        <ul>
+            <li><a href='https://uturnaudio.com/products/orbit-custom'>U-Turn Audio Orbit Custom Record Player</a>
+                <li style="list-style-type: none">
+                <ul>
+                    <li>Acrylic platter</li>
+                    <li><a href='https://uturnaudio.com/products/grado-black1'>Grado Black1 cartridge</a></li>
+                    <li><a href='https://uturnaudio.com/products/pluto-phono-preamp'>Built in Pluto Phono Preamp</a></li>
+                    <li><a href='/assets/images/2018-09-01-sound-system/donuts.gif'>45 RPM donut</a></li>
+                    <li>Some stupid little brush so I can feel good about cleaning every spec of dust off the records. I don't even know, man.</li>
+                </ul>
+                </li>
+            <li><a href='https://www.goldenear.com/products/aon-series'>GoldenEar Aon3 Speakers</a></li>
+            <li><a href=''>Amplifier</a> from Overture Audio in Ann Arbor, MI</li>
+            <li>Pear Anjou Speaker cables. Just kidding, we're not made of money or that stupid</li>
+        </ul>
+    </p>
 
     <h2>Raspberry Pi's</h2>
     <p>I bought two <a href='https://www.raspberrypi.org/products/'>Raspberry Pi 3 Model B+</a>'s and loaded a Debian based distro called <a href='https://volumio.org/'>Volumio</a> onto their SD cards.</p>
